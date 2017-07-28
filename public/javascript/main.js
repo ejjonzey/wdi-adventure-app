8fd57bf80f9a7f9a079c80dc473164abc8f32f62
whuizenga <will.huizenga@gmail.com>
2017-07-28 17:29:26 -0400

    added more images to the homepage

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -35,7 +35,7 @@ var rotateMarquee = function (index, curMarquee, deleteTime){
 }
 
 var slideshow = function (){
-    var slideshowList = ["/images/wasteland.jpg", "/images/lightbulbs.jpeg", "/images/tower.jpeg", "/images/hiking.jpeg", "/images/edgar.jpg"];
+    var slideshowList = ["/images/wasteland.jpg", "/images/lightbulbs.jpeg", "/images/tower.jpeg", "/images/hiking.jpeg", "/images/edgar.jpg", "/images/dining.jpg", "/images/greek.jpg", "images/white.jpg", "images/pines.jpeg"];
     var randomImage = Math.floor(Math.random()*slideshowList.length);
     console.log(randomImage);
     console.log(slideshowList[randomImage]);
