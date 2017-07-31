fc689be3a3ed31e621fc86db3c70063b14ed7669
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-31 14:17:13 -0400

    views edit

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/adventures', adventureController);
+app.use('/adventures/delete', adventureController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
